a180f21952eeb036b9eb3b8998a9042401db5a96
Vladyslav Todosiuk <vladyslav.todosiuk@student.tuke.sk>
2021-11-03 16:23:38 +0100

    Update DefectiveLight.java

--- src/main/java/sk/tuke/kpi/oop/game/DefectiveLight.java
+++ src/main/java/sk/tuke/kpi/oop/game/DefectiveLight.java
@@ -4,20 +4,28 @@ import org.jetbrains.annotations.NotNull;
 import sk.tuke.kpi.gamelib.Scene;
 import sk.tuke.kpi.gamelib.actions.Invoke;
 import sk.tuke.kpi.gamelib.framework.actions.Loop;
+import sk.tuke.kpi.gamelib.graphics.Animation;
 
 public class DefectiveLight extends Light implements Repairable {
     private int number;
 
+    private Animation lightOn;
+    private Animation lightOff;
+
     public DefectiveLight() {
         super();
+
+        lightOff = new Animation("sprites/light_off.png", 16, 16);
+        lightOn = new Animation("sprites/light_on.png", 16, 16);
     }
 
     public void randomNumber() {
         if (isOn() ) {
             number = (int) (Math.random() * 20);
             if (number == 5 || number == 10) {
-                setAnimation(lightOff);
-            } else setAnimation(lightOn);
+                setAnimation(lightOn);
+            }
+            else setAnimation(lightOff);
         }
     }
 
