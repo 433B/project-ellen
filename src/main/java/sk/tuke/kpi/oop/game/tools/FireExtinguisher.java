72e8c862f2d23736f985d64eb6f76e5a4e70e16d
Vladyslav Todosiuk <vladyslav.todosiuk@student.tuke.sk>
2021-11-04 20:26:49 +0100

    Update FireExtinguisher.java

--- src/main/java/sk/tuke/kpi/oop/game/tools/FireExtinguisher.java
+++ src/main/java/sk/tuke/kpi/oop/game/tools/FireExtinguisher.java
@@ -1,14 +1,26 @@
 package sk.tuke.kpi.oop.game.tools;
+
 import sk.tuke.kpi.gamelib.graphics.Animation;
+import sk.tuke.kpi.oop.game.Reactor;
 
-public class FireExtinguisher extends BreakableTool {
-
-    private Animation extinguisherAnimation;
+public class FireExtinguisher extends BreakableTool<Reactor>  {
+    public Animation extinguisherAnimation;
 
     public FireExtinguisher() {
         super(1);
-
         extinguisherAnimation = new Animation("sprites/extinguisher.png", 16, 16);
         setAnimation(extinguisherAnimation);
+    }
+
+    @Override
+    public void useWith(Reactor actor) {
+        if (actor == null) {
+            return;
+        }
+
+        if (actor.getDamage() == 100) {
+            actor.extinguih();
+            super.useWith(actor);
+        }
     }
 }
