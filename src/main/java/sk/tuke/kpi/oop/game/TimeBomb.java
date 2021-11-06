e66a50b9d0d6f66083f5571e2e0a303504f6dacb
Vladyslav <vladyslav.todosiuk@student.tuke.sk>
2021-11-07 01:55:10 +0200

    done

--- src/main/java/sk/tuke/kpi/oop/game/TimeBomb.java
+++ src/main/java/sk/tuke/kpi/oop/game/TimeBomb.java
@@ -3,8 +3,11 @@ package sk.tuke.kpi.oop.game;
 import sk.tuke.kpi.gamelib.actions.ActionSequence;
 import sk.tuke.kpi.gamelib.actions.Invoke;
 import sk.tuke.kpi.gamelib.actions.Wait;
+import sk.tuke.kpi.gamelib.actions.When;
 import sk.tuke.kpi.gamelib.framework.AbstractActor;
 import sk.tuke.kpi.gamelib.graphics.Animation;
+
+import java.util.Objects;
 
 
 public class TimeBomb extends AbstractActor {
@@ -22,55 +25,33 @@ public class TimeBomb extends AbstractActor {
     public void activate() {
         if (!isOn) {
             this.isOn = true;
-            setAnimation(new Animation("sprites/bomb_activated.png", 16, 16, 0.2f, Animation.PlayMode.LOOP_REVERSED));
+            setAnimation(new Animation("sprites/bomb_activated.png", 16, 16, time / 6, Animation.PlayMode.LOOP_REVERSED));
         }
         if (isOn) {
             this.time--;
             if (this.time == 0) {
-                setAnimation(new Animation("sprites/small_explosion.png", 16, 16, 0.25f, Animation.PlayMode.ONCE));
+                setAnimation(new Animation("sprites/small_explosion.png", 16, 16, 0.125f, Animation.PlayMode.ONCE));
                 new ActionSequence<>(
-                    new Wait<>(2),
+                    new Wait<>(time),
+                    new Invoke<>(this::setAnim),
                     new Invoke<>(this::delete)
                 ).scheduleFor(this);
             }
         }
     }
 
+    public void setAnim() {
+        setAnimation(new Animation("sprites/small_explosion.png", 16, 16, 0.125f, Animation.PlayMode.ONCE));
+    }
+
     public void delete() {
-        if (getScene() != null) {
-            getScene().removeActor(this);
-        }
+        new When<>(
+            () -> getAnimation().getCurrentFrameIndex() >= 7,
+            new Invoke<>(() -> Objects.requireNonNull(getScene()).removeActor(this))
+        ).scheduleFor(this);
     }
 
     public boolean isActivated() {
         return this.isOn;
     }
-
-//    public void detonation() {
-//        setAnimation(new Animation("sprites/small_explosion.png", 16, 16, 0.2f, Animation.PlayMode.ONCE));
-//        this.boom = true;
-//    }
-//
-//    public boolean isBoom() {
-//        return this.boom;
-//    }
-
-//    public void akBar() {
-//        if (isActivated()) {
-//            this.time--;
-//
-//            if (this.time == 0) {
-//                this.boom = true;
-//            }
-//            if (isBoom() && isActivated()) {
-//
-//            }
-//        }
-//    }
-
-//    public void waiting() {
-//        new ActionSequence<>(
-//            new Wait<>(2f)
-//        ).scheduleFor(this);
-//    }
 }
