13057aa3e970221b4b13ad9c7ebbb7b68ba17bd8
Vladyslav <vladyslav.todosiuk@student.tuke.sk>
2021-11-06 19:46:25 +0200

    done

--- src/main/java/sk/tuke/kpi/oop/game/TimeBomb.java
+++ src/main/java/sk/tuke/kpi/oop/game/TimeBomb.java
@@ -10,16 +10,19 @@ public class TimeBomb extends AbstractActor {
     private Animation boomAnimation;
 
     private boolean isOn;
+    private boolean boom;
     private float time;
 
     public TimeBomb(float time) {
         this.time = time;
         this.isOn = false;
+        this.boom = false;
 
         fireBombAnimation = new Animation("sprites/bomb_activated.png", 16, 16, 0.2f, Animation.PlayMode.LOOP_REVERSED);
-        boomAnimation = new Animation("sprites/small_explosion.png", 16, 16, 0.2f, Animation.PlayMode.LOOP_REVERSED);
+        boomAnimation = new Animation("sprites/small_explosion.png", 16, 16, 0.2f, Animation.PlayMode.ONCE);
         setAnimation(new Animation("sprites/bomb.png", 16, 16));
     }
+
 
     public void activate() {
         setAnimation(fireBombAnimation);
@@ -34,11 +37,16 @@ public class TimeBomb extends AbstractActor {
         if (isActivated()) {
             this.time--;
             if (this.time == 0) {
+                this.boom = true;
                 setAnimation(boomAnimation);
-            }
-            if (this.time == -20) {
+            } else this.boom = false;
+            if (this.time == -5) {
                 Objects.requireNonNull(getScene()).removeActor(this);
             }
         }
+    }
+
+    public boolean isBoom() {
+        return boom;
     }
 }
