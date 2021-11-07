b59fe8f379dd62ec168492180bf812e419c2f2bc
Vladyslav <vladyslav.todosiuk@student.tuke.sk>
2021-11-07 02:03:48 +0200

    done

--- src/main/java/sk/tuke/kpi/oop/game/TimeBomb.java
+++ src/main/java/sk/tuke/kpi/oop/game/TimeBomb.java
@@ -9,7 +9,6 @@ import sk.tuke.kpi.gamelib.graphics.Animation;
 
 import java.util.Objects;
 
-
 public class TimeBomb extends AbstractActor {
 
     private boolean isOn;
@@ -25,19 +24,14 @@ public class TimeBomb extends AbstractActor {
     public void activate() {
         if (!isOn) {
             this.isOn = true;
-            setAnimation(new Animation("sprites/bomb_activated.png", 16, 16, time / 6, Animation.PlayMode.LOOP_REVERSED));
+            new ActionSequence<>(new Wait<>(0), new Invoke<>(this::setKAK)).scheduleFor(this);
+            new ActionSequence<>(new Wait<>(time), new Invoke<>(this::setAnim), new Invoke<>(this::delete)).scheduleFor(this);
         }
-        if (isOn) {
-            this.time--;
-            if (this.time == 0) {
-                setAnimation(new Animation("sprites/small_explosion.png", 16, 16, 0.125f, Animation.PlayMode.ONCE));
-                new ActionSequence<>(
-                    new Wait<>(time),
-                    new Invoke<>(this::setAnim),
-                    new Invoke<>(this::delete)
-                ).scheduleFor(this);
-            }
-        }
+
+    }
+
+    public void setKAK() {
+        setAnimation(new Animation("sprites/bomb_activated.png", 16, 16, time / 6, Animation.PlayMode.LOOP_REVERSED));
     }
 
     public void setAnim() {
@@ -55,3 +49,20 @@ public class TimeBomb extends AbstractActor {
         return this.isOn;
     }
 }
+
+//        if (!isOn) {
+//            this.isOn = true;
+//            setAnimation(new Animation("sprites/bomb_activated.png", 16, 16, time / 6, Animation.PlayMode.LOOP_REVERSED));
+//        }
+//        if (isOn) {
+//            this.time--;
+//            if (this.time == 0) {
+//                setAnimation(new Animation("sprites/small_explosion.png", 16, 16, 0.125f, Animation.PlayMode.ONCE));
+//                new ActionSequence<>(
+//                    new Wait<>(time),
+//                    new Invoke<>(this::setAnim),
+//                    new Invoke<>(this::delete)
+//                ).scheduleFor(this);
+//            }
+//        }
+//}
