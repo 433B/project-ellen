f2fc4ca7022a64f64c45282f9ab0775206f2f917
Vladyslav <vladyslav.todosiuk@student.tuke.sk>
2021-11-26 22:16:08 +0200

    done

--- src/main/java/sk/tuke/kpi/oop/game/actions/Shift.java
+++ src/main/java/sk/tuke/kpi/oop/game/actions/Shift.java
@@ -3,7 +3,7 @@ package sk.tuke.kpi.oop.game.actions;
 import sk.tuke.kpi.gamelib.framework.actions.AbstractAction;
 import sk.tuke.kpi.oop.game.Keeper;
 
-public class Shift<A extends Keeper> extends AbstractAction<Keeper> {
+public class Shift<K extends Keeper> extends AbstractAction<K> {
 
     @Override
     public void execute(float deltaTime) {
