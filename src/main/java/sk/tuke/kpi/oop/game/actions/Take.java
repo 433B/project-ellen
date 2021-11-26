7940750399deba563b06b9ea10acc28fd1672f9c
Vladyslav <vladyslav.todosiuk@student.tuke.sk>
2021-11-26 22:36:08 +0200

    done

--- src/main/java/sk/tuke/kpi/oop/game/actions/Take.java
+++ src/main/java/sk/tuke/kpi/oop/game/actions/Take.java
@@ -1,38 +1,37 @@
 package sk.tuke.kpi.oop.game.actions;
 
 import sk.tuke.kpi.gamelib.Actor;
+import sk.tuke.kpi.gamelib.Scene;
 import sk.tuke.kpi.gamelib.framework.actions.AbstractAction;
 import sk.tuke.kpi.oop.game.Keeper;
 import sk.tuke.kpi.oop.game.items.Collectible;
 
+import java.util.List;
 import java.util.Objects;
 
-public class Take<A extends  Keeper> extends AbstractAction<Keeper> {
-    private A ripley;
-
-    public Take() {
-    }
-
-    public Take(A ripley) {
-        this.ripley = ripley;
-    }
+public class Take<A extends  Keeper> extends AbstractAction<A> {
 
     @Override
     public void execute(float deltaTime) {
-        if (getActor() != null || getActor().getScene() != null && !isDone()) {
-            try {
-                for (Actor weapon : Objects.requireNonNull(Objects.requireNonNull(getActor()).getScene()).getActors()) {
-                    if (weapon instanceof Collectible && weapon != this.ripley && getActor().intersects(weapon)) {
-                        getActor().getBackpack().add((Collectible) weapon);
-                        Objects.requireNonNull(getActor().getScene()).removeActor(weapon);
+        Scene scene = Objects.requireNonNull(getActor()).getScene();
+        List<Actor> itemList;
+
+        if (!isDone() && (getActor().getScene() == null || getActor() != null)) {
+            itemList = Objects.requireNonNull(getActor().getScene()).getActors();
+            for (Actor weapon : itemList) {
+                if (weapon instanceof Collectible && weapon.intersects(getActor())) {
+                    try {
+                        getActor().getBackpack().add(((Collectible) weapon));
+                        assert scene != null;
+                        scene.removeActor(weapon);
                         break;
+                    } catch (IllegalStateException exception) {
+                        assert scene != null;
+                        scene.getOverlay().drawText(exception.getMessage(), 0, 0).showFor(2);
                     }
                 }
-            } catch (IllegalStateException error) {
-                Objects.requireNonNull(Objects.requireNonNull(getActor()).getScene()).getOverlay().drawText(error.getMessage(), 0, 0).showFor(2);
-            } finally {
-                setDone(true);
             }
+            setDone(true);
         }
     }
 }
