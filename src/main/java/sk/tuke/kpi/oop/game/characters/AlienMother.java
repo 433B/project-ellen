92b7a50e975371bee48b2703690e243bb4be451a
Vladyslav <vladyslav.todosiuk@student.tuke.sk>
2021-11-27 23:31:29 +0200

    done

--- src/main/java/sk/tuke/kpi/oop/game/characters/AlienMother.java
+++ src/main/java/sk/tuke/kpi/oop/game/characters/AlienMother.java
@@ -5,8 +5,8 @@ import sk.tuke.kpi.oop.game.behaviours.RandomlyMoving;
 
 public class AlienMother extends Alien{
 
-    public AlienMother(int health, RandomlyMoving behaviour) {
-        super(health, behaviour);
+    public AlienMother(int health, RandomlyMoving randomlyMoving) {
+        super(health, randomlyMoving);
         setAnimation(new Animation("sprites/mother.png", 112, 162, 0.2f, Animation.PlayMode.LOOP_REVERSED));
     }
 }
