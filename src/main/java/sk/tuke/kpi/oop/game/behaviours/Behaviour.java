d9d43186155bbba85537846f2b72ae16ef5be046
Vladyslav <vladyslav.todosiuk@student.tuke.sk>
2021-11-27 23:21:30 +0200

    done

--- src/main/java/sk/tuke/kpi/oop/game/behaviours/Behaviour.java
+++ src/main/java/sk/tuke/kpi/oop/game/behaviours/Behaviour.java
@@ -3,7 +3,5 @@ package sk.tuke.kpi.oop.game.behaviours;
 import sk.tuke.kpi.gamelib.Actor;
 
 public interface Behaviour <A extends Actor> {
-    void setup(A actor);
-
-    void setUp(A alien);
+    void setUp(A actor);
 }
--- src/main/java/sk/tuke/kpi/oop/game/behaviours/Observing.java
+++ src/main/java/sk/tuke/kpi/oop/game/behaviours/Observing.java
@@ -1,5 +1,28 @@
 package sk.tuke.kpi.oop.game.behaviours;
 
-public class Observing {
+import sk.tuke.kpi.gamelib.Actor;
+import sk.tuke.kpi.gamelib.messages.Topic;
 
+import java.util.Objects;
+import java.util.function.Predicate;
+
+public class Observing <A extends Actor, T> implements Behaviour<A> {
+    private final Topic<T> obserTopic;
+    private final Predicate<T> observPred;
+    private final Behaviour<A> observDelegate;
+
+    public Observing(Topic<T> topic, Predicate<T> predicate, Behaviour<A> behaviour) {
+        this.obserTopic = topic;
+        this.observPred = predicate;
+        this.observDelegate = behaviour;
+    }
+
+    @Override
+    public void setUp(A actor) {
+        if (actor != null) {
+            Objects.requireNonNull(actor.getScene()).getMessageBus().subscribe(obserTopic, topicActor -> {
+                    if (observPred.test(topicActor)) observDelegate.setUp(actor);
+            });
+        }
+    }
 }
